9d941ba50aa6c5c85779071a6b2371c342137855
Ex1stence <32302269+Ex1stence@users.noreply.github.com>
2018-05-21 22:00:57 +0800

    bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,12 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+	if (message.content === 'pogi'){
+		message.reply('James Tan is pogi')
+
+	}
+})
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
